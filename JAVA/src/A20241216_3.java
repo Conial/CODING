17ccaab38c0f62a082a82904e6a4c5ce7a6a844f
conial <tokibusin002@gmail.com>
2024-12-16 17:37:15 +0900

    modified:   .vscode/launch.json 	new file:   JAVA/src/A20241216_3.java

---
+++ JAVA/src/A20241216_3.java
@@ -0,0 +1,13 @@
+public class A20241216_3 {
+    public static void main(String[] args) {
+        String YOON = "2.82";
+        double dobYOON = Double.parseDouble(YOON);
+        System.out.println(dobYOON);
+
+        int SEX = (int)dobYOON * 3;
+        int SEXY = (int)(dobYOON * 3);
+
+        System.out.println(SEX);
+        System.out.println(SEXY);
+    }
+}
