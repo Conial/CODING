90106277209bacc423eaad8b7164b64d73532f54
conial <tokibusin002@gmail.com>
2024-12-17 18:05:57 +0900

    modified:   JAVA/src/20241217/C.class 	modified:   JAVA/src/20241217/C.java 	new file:   JAVA/src/20241217/D.class 	new file:   JAVA/src/20241217/D.java 	new file:   JAVA/src/20241217/E.class 	new file:   JAVA/src/20241217/E.java 	new file:   JAVA/src/20241217/F.class 	new file:   JAVA/src/20241217/F.java

--- JAVA/src/20241217/C.java
+++ JAVA/src/20241217/C.java
@@ -9,9 +9,16 @@ public class C {
         while(true) {
             INPUT = SCAN.nextLine();
             System.out.println("입력된 문자열 : \"" + INPUT + "\"");
-            if(INPUT.equals("SEX")) {
+            if(INPUT.equals("gojjang061202")) {
+                System.out.println("암호와 일치합니다.");
+                System.out.println("잠금을 해제합니다.\n");
                 break;
             }
+            else {
+                System.out.println("암호가 틀렸습니다.\n");
+            }
         }
+
+        SCAN.close();
     }
 }
---
+++ JAVA/src/20241217/F.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+import javax.sound.sampled.AudioFormat.Encoding;
+
+public class F {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String Name, Front;
+        long Phone;
+
+        System.out.print("이름: ");
+        Name = scanner.nextLine();
+
+        System.out.print("생년월일: ");
+        Front = scanner.nextLine();
+
+        System.out.print("전번: ");
+        Phone = Integer.parseInt(scanner.nextLine());
+        
+        System.out.println(Name);
+        System.out.println(Front);
+        System.out.println((Phone / 100000000) + "-" + ((Phone % 1000000000) / 10000) + "-" + ((Phone % 100000000) % 10000));
+    }
+}
